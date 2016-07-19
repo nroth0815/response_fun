20c35fddabcb07b89fb0a30773957c62cdb436dd
nroth <n.roth@ucl.ac.uk>
2016-07-19 18:44:36 +0200

    Some new files

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test_pack',
+      version='0.1',
+      description='Some random tests',
+      url='http://github.com/nroth0815/response_fun',
+      author='Nina Roth',
+      author_email='nrmailfwd@gmail.com',
+      license='MIT',
+      packages=['test_pack'],
+      zip_safe=False)
